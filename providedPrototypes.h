f3abd7ad2aad423f618e31788fefbccd84ed24b8
Taylor Heimbichner <theimbichner@email.arizona.edu>
2017-11-28 15:40:52 -0700

    Updated testcases

--- providedPrototypes.h
+++ providedPrototypes.h
@@ -8,18 +8,30 @@
 
 #define PROVIDED_PROTOTYPES_H
 
-extern int  spawnReal(char *, int (*func)(char *), char *,
-                       int, int);
-extern int  waitReal(int *);
-extern void terminateReal(int);
-extern int  semcreateReal(int);
-extern int  sempReal(int);
-extern int  semvReal(int);
-extern int  semfreeReal(int);
-extern int  gettimeofdayReal(int *);
-extern int  cputimeReal(int *);
-extern int  getPID_real(int *);
+extern int  spawnReal(char *name, int (*func)(char *), char *arg,
+                       int stack_size, int priority);
+extern int  waitReal(int *status);
+extern void terminateReal(int exit_code);
+extern int  semcreateReal(int init_value);
+extern int  sempReal(int semaphore);
+extern int  semvReal(int semaphore);
+extern int  semfreeReal(int semaphore);
+extern int  gettimeofdayReal(int *time);
+extern int  cputimeReal(int *time);
+extern int  getPID_real(int *pid);
 
-extern void* vmInitReal(int ,int ,int ,int);
+extern int  diskSizeReal (int unit, int *sectorSize, int *trackSize,
+                          int *diskSize);
+extern int  diskReadReal (int unit, int track, int first_sector,
+                          int numSectors, void *buffer);
+extern int  diskWriteReal(int unit, int track, int first_sector,
+                          int numSectors, void *buffer);
+
+extern int  Mbox_Create(int numslots, int slotsize, int *mboxID);
+extern int  Mbox_Release(int mboxID);
+extern int  Mbox_Send(int mboxID, void *msgPtr, int msgSize);
+extern int  Mbox_Receive(int mboxID, void *msgPtr, int msgSize);
+extern int  Mbox_CondSend(int mboxID, void *msgPtr, int msgSize);
+extern int  Mbox_CondReceive(int mboxID, void *msgPtr, int msgSize);
 
 #endif  /* PROVIDED_PROTOTYPES_H */
